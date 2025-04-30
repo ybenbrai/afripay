d2ec8c8e54a586413ff12a742c9446ba3a34fa43
khimya <benbraitit@gmail.com>
2025-04-30 02:48:14 +0100

    Initial commit for servervv

--- src/lib/httpClient.ts
+++ src/lib/httpClient.ts
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 export const httpClient = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000",
+  baseURL:
+    process.env.NEXT_PUBLIC_API_URL ||
+    "https://afripay-backend-production.up.railway.app" ||
+    "http://localhost:8000",
   withCredentials: true,
   headers: {
     "Content-Type": "application/json",
